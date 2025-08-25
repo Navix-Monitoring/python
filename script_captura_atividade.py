691c94536f0b645c34aa9d83111b9029897dc7bc
IsaakGuilherme <isaak.guilherme@sptech.school>
2025-08-25 12:04:56 -0300

    Script base de captura

---
+++ script_captura_atividade.py
@@ -0,0 +1,24 @@
+import psutil
+import pandas as pd
+from datetime import datetime
+import time
+
+df_inicial = pd.DataFrame(columns=['timestamp', 'cpu', 'ram', 'disco'])
+df_inicial.to_csv("captura.csv", index=False)
+
+while True:
+    porcentagem_cpu = psutil.cpu_percent(interval=1)
+    porcentagem_ram = psutil.virtual_memory().percent
+    porcentagem_disco = psutil.disk_usage('/').percent
+    tempo_atual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    df = pd.DataFrame([{
+        'timestamp': tempo_atual,
+        'cpu': porcentagem_cpu,
+        'ram': porcentagem_ram,
+        'disco': porcentagem_disco
+    }])
+
+    df.to_csv('captura.csv', mode='a', index=False, header=False)
+
+    time.sleep(10)
